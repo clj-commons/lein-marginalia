dfac9b5d2d9001bde0887de4d34e63cdddab8523
zkim <zachary.kim@gmail.com>
2011-11-23 14:41:30 -0700

    Removed clojure dependency per https://github.com/fogus/lein-marginalia/pull/5#issuecomment-2853056

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
 (defproject lein-marginalia "0.7.0-SNAPSHOT"
   :description "Leiningen plugin for Marginalia (http://github.com/fogus/marginalia)."
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [marginalia "0.7.0-SNAPSHOT"]]
+  :dependencies [[marginalia "0.7.0-SNAPSHOT"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
