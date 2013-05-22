a65d4b8fdbaaba0e50f1692063fa7fd8a02f25d6
fogus <mefogus@gmail.com>
2013-05-22 08:56:08 -0400

    Using evail in lein

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject lein-marginalia "0.8.0-SNAPSHOT"
   :description "Leiningen plugin for Marginalia."
   :url "http://github.com/fogus/marginalia"
-  :dependencies [[marginalia "0.8.0-SNAPSHOT"]])
+  :dependencies [[marginalia "0.8.0-SNAPSHOT"]]
+  :eval-in :leiningen)
+
