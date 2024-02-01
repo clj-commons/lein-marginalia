ad082209e2c53c1ac5f6b81afa6b20f0c71dadb0
Sean Corfield <sean@corfield.org>
2024-01-31 22:38:15 -0800

    update project.clj for 0.9.2
    Signed-off-by: Sean Corfield <sean@corfield.org>

--- project.clj
+++ project.clj
@@ -1,5 +1,15 @@
 (defproject lein-marginalia "0.9.2"
   :description "Leiningen plugin for Marginalia."
-  :url "http://github.com/gdeer81/marginalia"
+  :url "http://github.com/clj-commons/marginalia"
+  :license {:name "Eclipse Public License - v 1.0"
+          :url "http://www.eclipse.org/legal/epl-v10.html"
+          :distribution :repo
+          :comments "same as Clojure"}
   :dependencies [[marginalia "0.9.2"]]
+
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
+                                     :sign-releases false}]
+                        ["snapshots" {:url "https://clojars.org/repo"
+                                      :sign-releases false}]]
+
   :eval-in :leiningen)
